6f00f1b0719b8bbabd1311a176f51cca14857d33
abhijeet <abhi001jeet@gmail.com>
2024-11-08 18:16:59 +0530

    attach two chains together with sequential chain

--- Chain.py
+++ Chain.py
@@ -1,25 +1,49 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain,SequentialChain
 from dotenv import load_dotenv
 
 load_dotenv()
 
 llm = OpenAI()
 
-code_promt = PromptTemplate(
+#Code Template
+code_prompt = PromptTemplate(
     template="Write a very short {langauge} function that will {task}",
     input_variables=["language","task"]
 )
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt=code_promt
+# Test Template
+test_prompt = PromptTemplate(
+    template="Write a test for following {langauge} code:\n {code}",
+    input_variables=["langauge","code"]
 )
 
-result = code_chain({
+
+# code chain
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt,
+    output_key="code"
+)
+
+# test chain
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain,test_chain],
+    input_variables=["task","langauge"],
+    output_variables=["code","test"]
+)
+
+
+result = chain({
     "langauge":"python",
     "task":'return a list of numbers'
 })
 
-print(result['text'])
+print(result)
